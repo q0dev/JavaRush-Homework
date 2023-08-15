efe51ae57c318f2c44c632fcb8a237bacbac75ad
EnCODeR <x3x3>
2023-08-15 21:57:48 +0400

    Создал статическое поле game, в main создал список объектов Horse и добавил 3 объекта в этот список, создал объект Hippodrome и сохранил его в поле game

--- Hippodrome.java
+++ Hippodrome.java
@@ -1,9 +1,11 @@
 package com.javarush.task.task21.task2113;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Hippodrome {
     private List<Horse> horses;
+    public static Hippodrome game;
 
     public Hippodrome(List<Horse> horses) {
         this.horses = horses;
@@ -14,6 +16,11 @@ public class Hippodrome {
     }
     
     public static void main(String[] args) {
+        List<Horse> horses = new ArrayList<>();
+        horses.add(new Horse("Stan", 3, 0));
+        horses.add(new Horse("Chuck", 3, 0));
+        horses.add(new Horse("Grag", 3, 0));
 
+        game = new Hippodrome(horses);
     }
 }
