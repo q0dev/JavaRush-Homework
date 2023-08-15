372eb7a7801a0a02a6f021d25402a37dadf21142
EnCODeR <x3x3>
2023-08-15 21:09:37 +0400

    В класс Horse добавил поля name, speed и distance

--- Horse.java
+++ Horse.java
@@ -1,4 +1,10 @@
 package com.javarush.task.task21.task2113;
 
+//Наши лошади будут бежать просто определенное время (100 секунд/"шагов").
+//Будем определять победителя, как лошадь, пробежавшую наибольшую дистанцию.
+
 public class Horse {
+    private String name;
+    private double speed;
+    private double distance;
 }
